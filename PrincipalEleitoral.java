d5b91aa47e771042010d956e9bab77cf785d86ea
Fernanda Freitas <125224240+fernandafreittas@users.noreply.github.com>
2023-05-01 13:05:08 -0400

    Questão 10

---
+++ PrincipalEleitoral.java
@@ -0,0 +1,13 @@
+package Teste;
+
+public class PrincipalEleitoral {
+
+	public static void main(String[] args) {
+		Eleitoral obj = new Eleitoral();
+		obj.idade = 65;
+		obj.nome = "Loira";
+		obj.imprimir();
+
+	}
+
+}
