38f68fc215d089de24649d7689da00c96e7e4ed7
Fernanda Freitas <125224240+fernandafreittas@users.noreply.github.com>
2023-05-23 11:43:31 -0400

    Aula

---
+++ TestaClasses.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JOptionPane;
+public class TestaClasses {
+
+	public static void main(String[] args) {
+		JPanel p1 = new JPanel();
+		p1.setVisible(true);
+		p1.setLayout(null);
+		
+
+	}
+
+}
